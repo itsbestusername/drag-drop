341330ff88a4dd12c0d75574eb1471c459bcc889
Ekaterina Kim <im.work-k@mail.ru>
2023-12-04 16:47:20 +0300

    add getRandomColor function

--- aim-game/app.js
+++ aim-game/app.js
@@ -3,6 +3,7 @@ const screens = document.querySelectorAll(".screen");
 const timeList = document.querySelector("#time-list");
 const timeEl = document.querySelector("#time");
 const board = document.querySelector("#board");
+const colors = ['red', 'blue', 'green', 'yellow', 'pink', 'grey', 'white'];
 let time = 0;
 let score = 0;
 
@@ -60,16 +61,22 @@ function createRandomCircle() {
   const { width, height } = board.getBoundingClientRect();
   const x = getRandomNumber(0, width - size);
   const y = getRandomNumber(0, height - size);
+  const color = getRandomColor()
 
   circle.classList.add("circle");
   circle.style.width = `${size}px`;
   circle.style.height = `${size}px`;
   circle.style.top = `${y}px`;
   circle.style.left = `${x}px`;
+  circle.style.background = color;
 
   board.append(circle);
 }
 
 function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min);
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
 }
